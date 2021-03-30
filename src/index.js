1f0233acd1d41a7b4407e6ac4b625a36c638aee3
palomaraujo <palomaraujo4@gmail.com>
2021-03-30 11:39:26 -0300

    Projeto inicial e card component

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Card from './Card';
+import 'tachyons';
+
+ReactDOM.render(
+  <Card />,
+  document.getElementById('root')
+);
+
+
